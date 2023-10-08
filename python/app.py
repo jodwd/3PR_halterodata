4c3f915a8f6e30a31ff47ccb39fe64a3cc2d2dbb
jodwd <joris.dawid@gmail.com>
2023-10-09 00:09:31 +0200

    Lot of small stuff regarding "responsive" design and visuals of the app and athlete page. Data update 2023-10-09

--- python/app.py
+++ python/app.py
@@ -1,5 +1,5 @@
 import dash
-from dash import html, dcc
+from dash import html, dcc, Input, Output, State, html
 import dash_bootstrap_components as dbc
 
 app = dash.Dash(__name__,  external_stylesheets=[dbc.themes.BOOTSTRAP],
@@ -21,17 +21,31 @@ app.layout = \
                            'color': 'White',
                            'backgroundColor': 'rgb(0, 0, 131)',
                            'font-family': 'Segoe UI'}),
-                    html.Div([
-                        dcc.Link(page['name']+"  |  ",
-                                 href=page['path'],
-                                 style = {'fontSize': 20,
-                                 'color': 'White',
-                                 'backgroundColor': 'rgba(0,0,0,0.5)',
-                                 'font-family': 'Segoe UI'}, className='links')
-                        for page in dash.page_registry.values()
-                        ],
-                        className='link_zone'
-                    ),
+
+                            html.Div([
+                                dbc.Button("Info", id="open", color="info", outline=True, className="me-1"),
+                                dbc.Modal([
+                                        dbc.ModalHeader("Information"),
+                                        dbc.ModalBody([
+                                            html.P("Développé par Joris Dawid (CH Arbresle)"),
+                                            html.P("📧 joris.dawid@gmail.com"),
+                                        ]),
+                                        dbc.ModalFooter(
+                                            dbc.Button("Close", id="close-button", color="secondary", className="ml-auto")
+                                        ),
+                                    ], id="info-modal", size="lg", centered=True, is_open=False),
+                            ], className='info_button'),
+                            html.Div([
+                                dcc.Link(page['name']+"  |  ",
+                                         href=page['path'],
+                                         style = {'fontSize': 20,
+                                         'color': 'White',
+                                         'backgroundColor': 'rgba(0,0,0,0.5)',
+                                         'font-family': 'Segoe UI'}, className='links')
+                                for page in dash.page_registry.values()
+                                ],
+                                className='link_zone'
+                            ),
                     html.Div(className='hr1'),
                     html.Div(className='hr2'),
                     html.Div(className='hr3'),
@@ -47,6 +61,17 @@ app.layout = \
     ]
 )
 
+@app.callback(
+    Output("info-modal", "is_open"),
+    [Input("open", "n_clicks"),
+    Input("close-button", "n_clicks")],
+    State("info-modal", "is_open"),
+)
+
+def toggle_modal(open_clicks, close_clicks, is_open):
+    if open_clicks or close_clicks:
+        return not is_open
+    return is_open
 
 if __name__ == "__main__":
     app.run(debug=True)
