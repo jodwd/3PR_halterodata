7d16fd386f76784ea5bf9fe8e1e38ef4a11db1c6
jodwd <joris.dawid@gmail.com>
2024-10-27 23:22:30 +0100

    Auto-executed scripts and pushed results

---
+++ update/auto_update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Step 1: Run the Ruby script
+#echo "Running Ruby script..."
+#ruby C:\Users\joris\PycharmProjects\halterodata\ruby\athletes_scoresheet.rb
+
+# Step 2: Run the Python script
+echo "Running Python script..."
+python3 C:\Users\joris\PycharmProjects\halterodata\data_transform.py
+
+
+# Step 3: Add changes to Git
+echo "Adding changes to Git..."
+cd C:\Users\joris\PycharmProjects\halterodata
+git checkout uat
+git add .
+
+# Step 4: Commit changes
+echo "Committing changes..."
+git commit -m "Auto-executed scripts and pushed results"
+
+# Step 5: Push to GitHub
+echo "Pushing to GitHub..."
+git push origin uat
