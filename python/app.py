2e8bed48f70b87c1a6ed3bb98a225ca5f4030fe1
jodwd <joris.dawid@gmail.com>
2025-05-26 09:03:16 +0200

    port binding test

--- python/app.py
+++ python/app.py
@@ -18,6 +18,9 @@ app = dash.Dash(__name__,  external_stylesheets=[dbc.themes.BOOTSTRAP],
 app.title = "3PR - Tableau de Bord de l'Haltérophilie en France"
 server = app.server
 
+#port
+port = int(os.environ.get('PORT', 3500))
+
 # Connection à la base SQLite
 dirname = os.path.dirname(os.path.abspath(__file__))
 path_db = os.path.join(dirname, 'pages/dataltero.db')
