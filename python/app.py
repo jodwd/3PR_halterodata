4b18b57904cd498270c896d83278f1e26904711c
jodwd <joris.dawid@gmail.com>
2026-06-15 10:34:06 +0200

    port binding fix test

--- python/app.py
+++ python/app.py
@@ -302,6 +302,5 @@ def anniv(is_open):
         return [txt_anniv]
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 3500))
-    app.run(host="0.0.0.0", port=port, debug=False)
+port = int(os.environ.get('PORT', 3500))
+app.run(host="0.0.0.0", port=port, debug=False)
